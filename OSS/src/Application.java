1985ba116e67205abbd0b19057f2da0a8eb3ae2a
dylan-polyu <116277909+dylan-polyu@users.noreply.github.com>
2023-11-28 21:22:10 +0800

    Add files via upload

--- OSS/src/Application.java
+++ OSS/src/Application.java
@@ -1,26 +1,39 @@
 import java.io.*;
-import java.io.Console;
 import java.sql.*;
 import java.util.Scanner;
-import oracle.jdbc.driver.*;
-import oracle.sql.*;
-
 
 public class Application {
     public static void main(String[] args) throws SQLException, IOException {
         Scanner scanner = new Scanner(System.in);
-        while(true){
-            System.out.println("Welcome to PolyShop, a friendly online shopping system. Please choose the following options: ");
-            System.out.println("1. login  2. create account");
+        OSS oss = new OSS();
+
+        while (true) {
+            System.out.println("========================================");
+            System.out.println("Welcome to PolyShop, a friendly online shopping system");
+            System.out.println("========================================");
+            System.out.println("Please choose from the following options:");
+            System.out.println("1. Login");
+            System.out.println("2. Create Account");
+            System.out.println("0. Close App");
+            System.out.print(">> ");
             String input = scanner.nextLine();
-            switch(input){
-                case "1":
-                    OSS.loginAccount();
-                case "2":
-                    OSS.createAccount();
+            System.out.println();
+            if (input.equals("0")) {
+                oss.closeApp();
+                break;
             }
+            switch (input) {
+                case "1":
+                    oss.loginAccount();
+                    break;
+                case "2":
+                    oss.createAccount();
+                    break;
+                default:
+                    System.out.println("Invalid input. Please enter a valid option.");
+                    break;
+            }
+            System.out.println();
         }
-
-
     }
 }
--- OSS/src/OSS.java
+++ OSS/src/OSS.java
@@ -9,54 +9,85 @@ public class OSS {
     private static Statement stmt;
     public static Statement getStmt() { return stmt; }
     public static void setStmt(Statement stmt) { OSS.stmt = stmt; }
+    OracleConnection conn;
+
+    public Scanner scanner;
     public OSS() throws SQLException, IOException{
-       DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-       OracleConnection conn = (OracleConnection)DriverManager.getConnection("jdbc:oracle:thin:@studora.comp.polyu.edu.hk:1521:dbms","\"22099885d\"","jnhhpagt");
-       setStmt(conn.createStatement());
+        scanner = new Scanner(System.in);
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        conn = (OracleConnection)DriverManager.getConnection("jdbc:oracle:thin:@studora.comp.polyu.edu.hk:1521:dbms","\"22099885d\"","jnhhpagt");
+        setStmt(conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE));
     }
-    public static void loginAccount() throws SQLException {
-        Scanner scanner = new Scanner(System.in);
-        String enteredUsername = "";
-        String enteredPassword = "";
-        String storedPassword = "";
-        ResultSet rset = null;
+    public void closeApp() throws SQLException {
+        conn.close();
+    }
+    public void loginAccount() throws SQLException {
+        String enteredUsername, enteredPassword;
+        ResultSet rset;
         do {
-            System.out.print("Input username: ");
+            System.out.print("Input your username: ");
             enteredUsername = scanner.next();
-            rset = getStmt().executeQuery("SELECT COUNT(*) FROM user where username = '" + enteredUsername + "'");
-            if(rset.getInt(1) == 0) {
-                System.out.println(enteredUsername + " does not exists.");
+            rset = getStmt().executeQuery("SELECT COUNT(*) FROM USERDATA WHERE userID = '" + enteredUsername + "'");
+            if (rset.next() && rset.getInt(1) >= 1) {
+                break;
+            } else {
+                System.out.println("Username does not exist!");
             }
-        } while (rset.getInt(1) == 0);
+        } while (true);
         do {
             System.out.print("Input password: ");
             enteredPassword = scanner.next();
-            rset = getStmt().executeQuery("SELECT password FROM user where username = '" + enteredUsername + "'");
-            storedPassword = rset.getString(1);
-            if(!enteredPassword.equals(storedPassword)) {
-                System.out.println("Incorrect password. Please try again.");
+            rset = getStmt().executeQuery("SELECT password FROM USERDATA WHERE userID = '" + enteredUsername + "'");
+            if (rset.next()) {
+                String storedPassword = rset.getString(1);
+                if (!enteredPassword.equals(storedPassword)) {
+                    System.out.println("Incorrect password. Please try again.");
+                } else {
+                    break;
+                }
             }
-        } while (!enteredPassword.equals(storedPassword));
+        } while (true);
     }
-    public static void createAccount() throws SQLException, IOException{
+    public void createAccount() throws SQLException, IOException {
         Scanner scanner = new Scanner(System.in);
-        String enteredUsername = "";
-        String enteredPassword = "";
-        ResultSet rset = null;
+        String enteredUsername, enteredPassword, firstName, lastName, dateOfBirth, email, phoneNumber, address;
+        ResultSet rset;
         do {
-            System.out.print("Input username: ");
+            boolean checkName = false;
+            System.out.print("Input unique username: ");
             enteredUsername = scanner.next();
-            rset = getStmt().executeQuery("SELECT COUNT(*) FROM user where username = '" + enteredUsername + "'");
-            if(rset.getInt(1) >= 1) {
-                System.out.println(enteredUsername + " already existed. Try another one.");
+
+            rset = getStmt().executeQuery("SELECT COUNT(*) FROM USERDATA WHERE userID = '" + enteredUsername + "'");
+            if (rset.next()) {
+                if (rset.getInt(1) >= 1) {
+                    System.out.println("Username already existed. Try another one.");
+                } else {
+                    checkName = true;
+                }
             }
-        } while (rset.getInt(1) >= 1);
+            if (checkName) {
+                break;
+            }
+        } while (true);
+        System.out.print("First name: ");
+        firstName = scanner.next();
+        System.out.print("Last name: ");
+        lastName = scanner.next();
+        System.out.print("Date of Birth: ");
+        dateOfBirth = scanner.next();
+        System.out.print("Email: ");
+        email = scanner.next();
+        System.out.print("Address: ");
+        address = scanner.next();
+        System.out.print("Phone number: ");
+        phoneNumber = scanner.next();
         System.out.print("Input password: ");
         enteredPassword = scanner.next();
-        getStmt().execute("INSERT INTO user\nVALUES('" + enteredUsername + "', '" + enteredPassword + "')");
-        System.out.println("User account created.");
+        getStmt().execute(String.format("INSERT INTO USERDATA VALUES('%s','%s','%s','%s',to_date('%s','YYYY-MM-DD'),'%s','%s')", enteredUsername, firstName, lastName, enteredPassword, dateOfBirth, email, phoneNumber));
+        getStmt().execute(String.format("INSERT INTO USERADDRESSES VALUES('%s','%s')", enteredUsername, address));
+        System.out.println("The account " + enteredUsername + " has been successfully created.");
     }
-    public static String[] search(String keyword) throws SQLException, IOException {
+    public String[] search(String keyword) throws SQLException, IOException {
         ResultSet rset = getStmt().executeQuery("SELECT * FROM products WHERE name LIKE %" + keyword + "% OR description LIKE %" + keyword + "%");
         while(rset.next()) {
             System.out.println(rset.getString(1));
